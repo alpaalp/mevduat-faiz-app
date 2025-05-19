1b44c63e4f9149a08c2d48fb0d7fe10d4fc27e3c
alpaalp <alperenb2747@gmail.com>
2025-05-20 02:13:02 +0300

    aaa

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import streamlit as st
+from scraping import get_faiz_tablosu
+
+st.set_page_config(page_title="Türkiye Bankaları Güncel Faiz Takibi", layout="wide")
+st.title("📊 Güncel Mevduat Faiz Oranları")
+
+kategori = st.selectbox("Kategori seçin", ["Hepsi", "32-91 günlük max oran", "92 günlük max oran", "günlük faiz"])
+df = get_faiz_tablosu()
+
+if kategori != "Hepsi":
+    df = df[["Banka", kategori]].sort_values(by=kategori, ascending=False)
+
+st.dataframe(df, use_container_width=True)
