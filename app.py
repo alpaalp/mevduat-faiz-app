daf6179d336151356f25a51f58273158843fe9eb
alpaalp <148859033+alpaalp@users.noreply.github.com>
2025-05-20 03:40:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 
 import streamlit as st
-from faiz_tablosu import create_faiz_tablosu
+from faiz_tablosu import get_faiz_tablosu
 
 st.set_page_config(page_title="Türkiye Bankaları Güncel Faiz Takibi", layout="wide")
 st.title("📊 Güncel Mevduat Faiz Oranları")
 
 kategori = st.selectbox("Kategori seçin", ["Hepsi", "32-91 günlük max oran", "92 günlük max oran", "günlük faiz"])
-df = create_faiz_tablosu()
+df = get_faiz_tablosu()
 
 if kategori != "Hepsi":
     df = df[["Banka", kategori]].sort_values(by=kategori, ascending=False)
