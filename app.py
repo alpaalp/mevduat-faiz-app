302c18338c0a42fdef86b3a16e4f49a964ad92f8
alpaalp <148859033+alpaalp@users.noreply.github.com>
2025-05-20 03:38:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Türkiye Bankaları Güncel Faiz Takibi", layout=
 st.title("📊 Güncel Mevduat Faiz Oranları")
 
 kategori = st.selectbox("Kategori seçin", ["Hepsi", "32-91 günlük max oran", "92 günlük max oran", "günlük faiz"])
-df = get_faiz_tablosu()
+df = create_faiz_tablosu()
 
 if kategori != "Hepsi":
     df = df[["Banka", kategori]].sort_values(by=kategori, ascending=False)
